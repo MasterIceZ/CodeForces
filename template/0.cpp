2eae0d5a288e91c1a7d207c9db49e47f166a14fc
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-05 20:49:32 +0700

    First Commit

---
+++ template/0.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1e9
+#define FOR(a,b,c) for(a=b;b<c;a++)
+#define ROF(a,b,c) for(a=b;a>c;a--)
+#define pq priority_queue
+#define pii pair<int,int>
+#define vi vector<int>
+#define ll long long
+#define quicksum(a,b) dp[a] += dp[b]
+#define arr array
+#define ld long double
+
+void solution()
+{
+	int i;
+
+	return ;
+}
+
+int main ()
+{
+	int q;
+	q = 1;
+	while(q--)
+	{
+		solution();
+	}
+	return 0;
+}

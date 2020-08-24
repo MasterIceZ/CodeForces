f14766cb44caff9388e52111b7d52776fc263fce
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-24 11:05:25 +0700

    Black Square

---
+++ Black_Square/main.cpp
@@ -0,0 +1,82 @@
+/*
+ * AUTHOR : _IceBorworntat926
+ * OS     : Ubuntu
+ * TASK   : CodeForces
+ * */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define space " "
+#define endl '\n'
+#define dec(a,b) fixed<<setprecision(b)<<a 
+
+template <class A> void read(vector<A>& v);
+template <class A,size_t S> void read(array <A, S>& a);
+template <class T> void read(T& x)
+{
+	cin >> x;
+}
+void read(double& d)
+{
+	string t;
+	read(t);
+	d = stod(t);
+}
+void read(long double& d)
+{
+	string t;
+	read(t);
+	d = stold(t);
+}
+template <class H,class... T> void read(H& h,T&... t)
+{
+	read(h);
+	read(t...);
+}
+template <class A> void read(vector <A>& x)
+{
+	for(auto& a : x)
+	{
+		read(a);
+	}
+}
+template <class A,size_t S> void read(array<A, S>& x)
+{
+	for(auto& a : x)
+	{
+		read(x);
+	}
+}
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int a,b,c,d;
+	read(a,b,c,d);
+	string s;
+	int ans = 0;
+	read(s);
+	for(int i=0;i<s.length();i++)
+	{
+		switch(s[i])
+		{
+			case '1' :
+				ans+=a;
+				break;
+			case '2' :
+				ans+=b;
+				break;
+			case '3' :
+				ans+=c;
+				break;
+			case '4' :
+				ans+=d;
+				break;
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}

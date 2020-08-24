869c146b749714db2cfb01d7767d2762d2b85b89
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-24 08:18:19 +0700

    Suborrays

---
+++ Suborrays/main.cpp
@@ -0,0 +1,71 @@
+/*
+ * AUTHOR : _IceBorworntat926
+ * OS     : Ubuntu
+ * TASK   : Beta.Programming.in.th
+ * */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define space " "
+#define endl '\n'
+#define dec(a,b) fixed<<setprecision(b)<<a 
+
+template <class A> void read(vector<A>& v);
+template <class A,size_t S> void read(array <A, S>& a);
+template <class T> void read(T& x)
+{
+	cin >> x;
+}
+void read(double& d)
+{
+	string t;
+	read(t);
+	d = stod(t);
+}
+void read(long double& d)
+{
+	string t;
+	read(t);
+	d = stold(t);
+}
+template <class H,class... T> void read(H& h,T&... t)
+{
+	read(h);
+	read(t...);
+}
+template <class A> void read(vector <A>& x)
+{
+	for(auto& a : x)
+	{
+		read(a);
+	}
+}
+template <class A,size_t S> void read(array<A, S>& x)
+{
+	for(auto& a : x)
+	{
+		read(x);
+	}
+}
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int a,n,q;
+	read(q);
+	while(q--)
+	{
+		read(n);
+		vector <int> v(n);
+		iota(all(v),1);
+		for(int i=0;i<n;i++)
+		{
+			cout << v[i] << space;	
+		}
+		cout << endl;
+	}
+	return 0;
+}

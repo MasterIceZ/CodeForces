7af4db4681f7dea236919663f422941a327febee
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-24 16:01:07 +0700

    Distance and Axis

---
+++ Distance_and_Axis/main.cpp
@@ -0,0 +1,80 @@
+/*
+ * AUTHOR : _IceBorworntat926
+ * OS     : Ubuntu
+ * TASK   : CodeForces
+ * */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define space " "
+#define endl '\n'
+#define dec(a,b) fixed<<setprecision(b)<<a 
+
+template <class A> void read(vector<A>& v);
+template <class A,size_t S> void read(array <A, S>& a);
+template <class T> void read(T& x)
+{
+	cin >> x;
+}
+void read(double& d)
+{
+	string t;
+	read(t);
+	d = stod(t);
+}
+void read(long double& d)
+{
+	string t;
+	read(t);
+	d = stold(t);
+}
+template <class H,class... T> void read(H& h,T&... t)
+{
+	read(h);
+	read(t...);
+}
+template <class A> void read(vector <A>& x)
+{
+	for(auto& a : x)
+	{
+		read(a);
+	}
+}
+template <class A,size_t S> void read(array<A, S>& x)
+{
+	for(auto& a : x)
+	{
+		read(x);
+	}
+}
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n,k,q;
+	read(q);
+	while(q--)
+	{
+		read(n,k);
+		if(k>=n)
+		{
+			cout << k-n << endl;
+		}
+		else if(n%2==0&&k%2==1)
+		{
+			cout << "1" << endl;
+		}
+		else if(n%2==1&&k%2==0)
+		{
+			cout << "1" << endl;
+		}
+		else
+		{
+			cout << "0" << endl;
+		}
+	}
+	return 0;
+}

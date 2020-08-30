01de35839d134bb92abf62c08549eda3414c3e7e
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-30 14:24:11 +0700

    Team

---
+++ Team/main.cpp
@@ -0,0 +1,71 @@
+/*
+ * AUTHOR : _IceBorworntat926
+ * OS     : Ubuntu
+ * TASK   : CodeForces
+ * */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define space " "
+#define endl '\n'
+#define dec(a,b) fixed<<setprecision(b)<<a 
+
+template <class A> void read(vector<A>& v);
+template <class A,size_t S> void read(array <A, S>& a);
+template <class T> void read(T& x)
+{
+	cin >> x;
+}
+void read(double& d)
+{
+	string t;
+	read(t);
+	d = stod(t);
+}
+void read(long double& d)
+{
+	string t;
+	read(t);
+	d = stold(t);
+}
+template <class H,class... T> void read(H& h,T&... t)
+{
+	read(h);
+	read(t...);
+}
+template <class A> void read(vector <A>& x)
+{
+	for(auto& a : x)
+	{
+		read(a);
+	}
+}
+template <class A,size_t S> void read(array<A, S>& x)
+{
+	for(auto& a : x)
+	{
+		read(x);
+	}
+}
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int T;
+	int cnt = 0;
+	read(T);
+	int a,b,c;
+	for(int i=0;i<T;++i)
+	{
+		read(a,b,c);
+		if(a+b+c>=T)
+		{
+			cnt++;
+		}
+	}
+	cout << cnt << endl;
+	return 0;
+}

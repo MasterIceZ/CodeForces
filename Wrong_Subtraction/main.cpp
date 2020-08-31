976011e1a2bac13aa7f40eec06c6bf914e4c3d84
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-08-31 11:11:11 +0700

    Wrong Subtraction

---
+++ Wrong_Subtraction/main.cpp
@@ -0,0 +1,72 @@
+/*
+ * AUTHOR : _IceBorworntat926
+ * OS     : Ubuntu
+ * TASK   : CodeForces
+ * */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(),(x).end()
+#define space " "
+#define endl '\n'
+#define dec(a,b) fixed<<setprecision(b)<<a 
+
+template <class A> void read(vector<A>& v);
+template <class A,size_t S> void read(array <A, S>& a);
+template <class T> void read(T& x)
+{
+	cin >> x;
+}
+void read(double& d)
+{
+	string t;
+	read(t);
+	d = stod(t);
+}
+void read(long double& d)
+{
+	string t;
+	read(t);
+	d = stold(t);
+}
+template <class H,class... T> void read(H& h,T&... t)
+{
+	read(h);
+	read(t...);
+}
+template <class A> void read(vector <A>& x)
+{
+	for(auto& a : x)
+	{
+		read(a);
+	}
+}
+template <class A,size_t S> void read(array<A, S>& x)
+{
+	for(auto& a : x)
+	{
+		read(x);
+	}
+}
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int n,k;
+	read(n,k);
+	for(int i=0;i<k;++i)
+	{
+		if(n%10==0)
+		{
+			n /= 10;
+		}
+		else
+		{
+			n--;
+		}
+	}
+	cout << n << endl;
+	return 0;
+}

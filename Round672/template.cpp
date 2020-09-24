5da744f52f820471255bde4bcf47cd4e20f667d2
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-09-24 21:02:24 +0700

    CodeForces Round 672

---
+++ Round672/template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define V_IN(x,v) for(auto& x : v)
+#define V_OUT(x,v) for(auto x : v)
+bool Q = false;
+void solution(){
+    
+}
+
+int32_t main (){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int T;
+    if(!Q){
+        solution();
+        return 0;
+    }
+    for(cin >> T;T--;){
+        solution();
+    }
+    return 0;
+}

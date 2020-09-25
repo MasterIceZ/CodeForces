cce7b5ae016b7cc8df7476e2f28832479003d15b
Borworntat Dendumrongkul <mastericez890@gmail.com>
2020-09-25 21:33:40 +0700

    Bit++

---
+++ Bit++/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int32_t main (){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int n = 0;
+    int q;
+    cin >> q;
+    for(int i=0;i<q;++i){
+        string s;
+        cin >> s;
+        if(i==q-1){
+            if(s=="X++" || s=="X--"){
+                cout << n << endl;
+            }
+            else if(s=="++X"){
+                cout << ++n << endl;
+            }
+            else
+            {
+                cout << --n << endl;
+            }
+        }
+        else{
+            if(s=="X++" || s=="++X"){
+                n++;
+            }
+            else{
+                n--;
+            }
+        }
+    }
+    return 0;
+}
